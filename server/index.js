3f87653960a1babe133f81b47cf12f14aa0199da
liwencheng <350718011@qq.com>
2018-11-07 14:49:13 +0800

    服务器连接数据库完毕

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 import koa from 'koa'
+import Router from 'koa-router'
+import cors from 'koa-cors'
+import bodyParser from 'koa-bodyparser'
+//引入路由文件
+import router from './routes/index'
+//连接数据库
+import query from './utils/query'
+
 const app = new koa()
+app.use(cors()).use(bodyParser()).use(router.routes())
+
 app.listen(3000,()=>{
     console.log('node is ok')
 })
